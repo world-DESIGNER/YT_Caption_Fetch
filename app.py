75d16b5eba78460fe5279023fb0e7d1af6900eac
world-DESIGNER <97209446+world-DESIGNER@users.noreply.github.com>
2023-11-08 08:58:10 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,23 @@
 from youtube_transcript_api import YouTubeTranscriptApi
 from youtube_transcript_api.formatters import SRTFormatter
-from flask import Flask, request, jsonify, send_from_directory
+from flask import Flask, request, jsonify
 from waitress import serve
-import re
 
 app = Flask(__name__)
 
 @app.route('/captions', methods=['GET'])
 def fetch_captions():
-    url = request.args.get('url')
-    format_type = request.args.get('format', 'text')  # default to 'text' if no format is provided
-    transcript_type = request.args.get('type', 'manual')  # default to 'manual' if no type is provided
-    lang = request.args.get('lang')  # no default value
+    video_id = request.args.get('video_id')
+    format_type = request.args.get('format', 'text')
+    transcript_type = request.args.get('type', 'manual')
+    lang = request.args.get('lang')
 
-    # Check if lang parameter is provided
     if not lang:
         return jsonify({"error": "No language provided. Please provide a language code with the 'lang' parameter."}), 400
+    if not video_id:
+        return jsonify({"error": "No video ID provided. Please provide a video ID with the 'video_id' parameter."}), 400
 
     try:
-        video_id = re.search(r"(?<=v=)[^&#]+|(?<=be/)[^&#]+", url)
-        video_id = video_id.group()
-
         transcript_list = YouTubeTranscriptApi.list_transcripts(video_id)
         transcripts = {}
 
@@ -28,7 +25,7 @@ def fetch_captions():
             if (transcript_type == 'auto' and not transcript.is_generated) or (transcript_type == 'manual' and transcript.is_generated):
                 continue
 
-            if transcript.language_code != lang:  # Skip if the transcript's language does not match the requested language
+            if transcript.language_code != lang:
                 continue
 
             lang_transcript = transcript.fetch()
@@ -36,7 +33,7 @@ def fetch_captions():
             if format_type.lower() == 'srt':
                 formatter = SRTFormatter()
                 formatted_transcript = formatter.format_transcript(lang_transcript)
-            else:  # if format is not 'srt', just return the captions
+            else:
                 formatted_transcript = [t['text'] for t in lang_transcript]
 
             transcripts[transcript.language_code] = formatted_transcript
@@ -48,15 +45,13 @@ def fetch_captions():
 
 @app.route('/available_languages', methods=['GET'])
 def fetch_available_languages():
-    url = request.args.get('url')
+    video_id = request.args.get('video_id')
+
+    if not video_id:
+        return jsonify({"error": "No video ID provided. Please provide a video ID with the 'video_id' parameter."}), 400
 
     try:
-        video_id = re.search(r"(?<=v=)[^&#]+|(?<=be/)[^&#]+", url)
-        video_id = video_id.group()
-
         transcript_list = YouTubeTranscriptApi.list_transcripts(video_id)
-
-        # Create a set to ensure no duplicate language codes
         language_codes = set()
 
         for transcript in transcript_list:
