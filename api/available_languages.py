f4311671c25e6b430d3a0bc9c73977dcf7261988
world-DESIGNER <yanghyunmin05@gmail.com>
2023-11-07 16:06:49 +0900

    Initial commit

---
+++ api/available_languages.py
@@ -0,0 +1,4 @@
+
+def fetch_available_languages(request):
+    # Your code to process the request and fetch available languages goes here.
+    pass
---
+++ api/captions.py
@@ -0,0 +1,4 @@
+
+def fetch_captions(request):
+    # Your code to process the request and fetch captions goes here.
+    pass
